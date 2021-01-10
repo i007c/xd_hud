385a6655b90adf79612460e488aa163f98b1b41d
i007c <dr007cc@gmail.com>
2021-01-10 18:19:25 +0330

    add set job and set gang funcs

--- ui/scripts/script.js
+++ ui/scripts/script.js
@@ -6,3 +6,31 @@ $(document).ready(function(){
     });
 })
 
+
+var job_kays = {
+    "LSPD": "police.png"
+};
+
+var job_imgs_directory = "img/"
+
+function set_job(jobkay) {
+    let job_img = job_kays[jobkay];
+    $(".job_img").css("background-image", "url("+ job_imgs_directory + job_img +")");
+}
+
+
+
+var gang_kays = {
+    "LSPD": "police.png"
+};
+
+var gang_imgs_directory = "img/"
+
+function set_gang(gangkay) {
+    let gang_img = gang_kays[gangkay];
+    $(".gang_img").css("background-image", "url("+ gang_imgs_directory + gang_img +")");
+}
+
+
+set_job("LSPD")
+set_gang("LSPD")
