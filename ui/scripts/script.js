ff4d97703ec3873c895d92eb5377b793f800bf33
i007c <dr007cc@gmail.com>
2021-01-10 18:58:43 +0330

    add setPlayerData and other funcs

--- ui/scripts/script.js
+++ ui/scripts/script.js
@@ -11,20 +11,19 @@ var job_kays = {
     "LSPD": "police.png"
 };
 
-var job_imgs_directory = "img/"
-
-function set_job(jobkay) {
-    let job_img = job_kays[jobkay];
-    $(".job_img").css("background-image", "url("+ job_imgs_directory + job_img +")");
-}
-
-
 
 var gang_kays = {
     "LSPD": "police.png"
 };
 
-var gang_imgs_directory = "img/"
+
+var job_imgs_directory = "img/";
+var gang_imgs_directory = "img/";
+
+function set_job(jobkay) {
+    let job_img = job_kays[jobkay];
+    $(".job_img").css("background-image", "url("+ job_imgs_directory + job_img +")");
+}
 
 function set_gang(gangkay) {
     let gang_img = gang_kays[gangkay];
@@ -32,5 +31,13 @@ function set_gang(gangkay) {
 }
 
 
-set_job("LSPD")
-set_gang("LSPD")
+function setPlayerDatas() {
+    $("#health_status").css("height", "67%");
+    $("#armor_status").css("height", "44%");
+    $("#thirst_status").css("height", "100%");
+    $("#hunger_status").css("height", "47%");
+    set_job("LSPD");
+    set_gang("LSPD");
+}
+
+setPlayerDatas();
