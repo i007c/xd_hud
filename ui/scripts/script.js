b63f60e7100f110d8a33a8e571bb2759dfaa4a0d
i007c <dr007cc@gmail.com>
2021-01-10 16:14:38 +0330

    add ui pages and update git ignore

---
+++ ui/scripts/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".btn").click(function(){
+        $(".HUD").toggleClass("active");
+        $(".icon").toggleClass("active");
+        $(".textdata").toggleClass("active");
+    });
+})
+
